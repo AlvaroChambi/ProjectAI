9ee5cf034e471d209207a3a453ff1cccea007351
Pablo Luis Gutierrez <pablo.gutierrez.utande@gmail.com>
2016-05-17 16:02:48 +0200

    Minimax modified

---
+++ ProjectWar/InfluenceMap.h
@@ -0,0 +1,14 @@
+//
+//  InfluenceMap.hpp
+//  ProjectWar
+//
+//  Created by Pablo Luis Gutierrez on 05/05/16.
+//  Copyright © 2016 Alvaro Chambi Campos. All rights reserved.
+//
+
+#ifndef InfluenceMap_hpp
+#define InfluenceMap_hpp
+
+#include <stdio.h>
+
+#endif /* InfluenceMap_hpp */

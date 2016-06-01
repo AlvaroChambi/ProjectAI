a342bf5430fddd8c1cd76d5517645f8d4926e668
achambi <alvaro.chambi.campos@gmail.com>
2016-06-01 22:42:03 +0200

    Cleaned unused class

--- ProjectWar/BoundingArea.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  BoundingArea.cpp
-//  ProjectWar
-//
-//  Created by Alvaro Chambi Campos on 01/06/16.
-//  Copyright (c) 2016 Alvaro Chambi Campos. All rights reserved.
-//
-
-#include "BoundingArea.h"

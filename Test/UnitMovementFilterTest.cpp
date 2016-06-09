64e6d88d5f8c8f9faf8c1335071008ca6956ed7b
achambi <alvaro.chambi.campos@gmail.com>
2016-06-09 20:07:08 +0200

    Cleaned unused tests

--- Test/UnitMovementFilterTest.cpp
+++ Test/UnitMovementFilterTest.cpp
@@ -26,37 +26,4 @@ public:
         
     }
     
-    UnitMovementFilter* movementFilter;
-    MockIterator iterator;
-    MockMap map;
 };
-
-TEST_F( UnitMovementFilterTest, IsInvalidatedEmptyInvalidatedList ) {
-    Unit* unit = new Unit;
-    movementFilter = new UnitMovementFilter( &iterator, &map, unit );
-    
-    ASSERT_FALSE( movementFilter->isInvalidated( Point( 0, 0 ) ) );
-    ASSERT_EQ( 1, movementFilter->getInvalidatedPositions().size() );
-}
-
-TEST_F( UnitMovementFilterTest, IsInvalidatedItemInList ) {
-    Unit* unit = new Unit;
-    movementFilter = new UnitMovementFilter( &iterator, &map, unit );
-    std::vector<Point> invalidated;
-    invalidated.push_back( Point( 1, 1 ) );
-    movementFilter->setInvalidatedPositions( invalidated );
-    
-    ASSERT_TRUE( movementFilter->isInvalidated( Point( 1, 1 ) ) );
-    ASSERT_EQ( 1, movementFilter->getInvalidatedPositions().size() );
-}
-
-TEST_F( UnitMovementFilterTest, IsInvalidatedNewItem ) {
-    Unit* unit = new Unit;
-    movementFilter = new UnitMovementFilter( &iterator, &map, unit );
-    std::vector<Point> invalidated;
-    invalidated.push_back( Point( 1, 1 ) );
-    movementFilter->setInvalidatedPositions( invalidated );
-    
-    ASSERT_FALSE( movementFilter->isInvalidated( Point( 2, 2 ) ) );
-    ASSERT_EQ( 2, movementFilter->getInvalidatedPositions().size() );
-}
